5f4ec5d1c467b47923adf03f681973b461f89f50
MeftunH <maftunhashimli@gmail.com>
2020-12-08 22:09:19 +0400

    add model

---
+++ FilmProjesi/Models/Salon.cs
@@ -0,0 +1,10 @@
+﻿namespace FilmProjesi.Models
+{
+    public class Salon
+    {
+        public int Id { get; set; }
+        public string SalonName { get; set; }
+        public int? CinemaId { get; set; }
+        public int? Capacity { get; set; }
+    }
+}
